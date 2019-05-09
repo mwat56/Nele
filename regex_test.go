e6cf5a1d455a3fe7fba2cb23c0d410dbd9471f6d
Matthias Watermann <support@mwat.de>
2019-05-09 20:35:10 +0200

    + implemented `TestMDtoHTML()` and `Test_handlePreCode()`;

--- regex_test.go
+++ regex_test.go
@@ -24,6 +24,55 @@ func Test_initWSre(t *testing.T) {
 	}
 } // Test_initWSre()
 
+func TestMDtoHTML(t *testing.T) {
+	m1 := []byte(`# head
+
+a single paragraph[^1].
+
+another "test" paragraph.
+
+    some preformatted
+	text
+
+[^1]: the footnote text
+`)
+	w1 := []byte(`<h1>head</h1>
+
+<p>a single paragraph<sup class="footnote-ref" id="fnref:1"><a href="#fn:1">1</a></sup>.</p>
+
+<p>another &ldquo;test&rdquo; paragraph.</p><pre>
+some preformatted
+text
+</pre><div class="footnotes">
+
+<hr>
+
+<ol>
+<li id="fn:1">the footnote text <a class="footnote-return" href="#fnref:1"><sup>[return]</sup></a></li>
+</ol>
+
+</div>
+`)
+	type args struct {
+		aMarkdown []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		// TODO: Add test cases.
+		{" 1", args{m1}, w1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MDtoHTML(tt.args.aMarkdown); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MDtoHTML() = %s, want %s", got, tt.want)
+			}
+		})
+	}
+} // TestMDtoHTML()
+
 func TestRemoveWhiteSpace(t *testing.T) {
 	txtIn1 := []byte(`<hr />
 	<p>Here is an example of AppleScript:</p>
@@ -216,3 +265,49 @@ func Test_getYMD(t *testing.T) {
 		})
 	}
 } // Test_getYMD()
+
+func Test_handlePreCode(t *testing.T) {
+	m1 := []byte(`<p>test</p>
+
+	<pre><code>pre
+part</code></pre>
+
+<p>line 2</p>
+`)
+	w1 := []byte(`<p>test</p><pre>
+pre
+part
+</pre><p>line 2</p>
+`)
+	m2 := []byte(`<p>test</p>
+
+	<pre><code class="language-go">pre
+part</code></pre>
+
+<p>line 2</p>
+`)
+	w2 := []byte(`<p>test</p><pre class="language-go">
+pre
+part
+</pre><p>line 2</p>
+`)
+	type args struct {
+		aMarkdown []byte
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantRHTML []byte
+	}{
+		// TODO: Add test cases.
+		{" 1", args{m1}, w1},
+		{" 2", args{m2}, w2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRHTML := handlePreCode(tt.args.aMarkdown); !reflect.DeepEqual(gotRHTML, tt.wantRHTML) {
+				t.Errorf("handlePreCode() = %s, want %s", gotRHTML, tt.wantRHTML)
+			}
+		})
+	}
+} // Test_handlePreCode()
