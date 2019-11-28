27eda14c6f4a83abc6e0cac4d957c2a5841caf6c
Matthias Watermann <support@mwat.de>
2019-11-28 19:53:16 +0100

    * numerous changes to add/set/update page preview links;

--- pageview.go
+++ pageview.go
@@ -10,6 +10,7 @@ package nele
 
 import (
 	"fmt"
+	"log"
 	"path/filepath"
 	"regexp"
 
@@ -18,78 +19,154 @@ import (
 )
 
 var (
+	// R/O RegEx to find an URL for the preview image's.
+	pvImageRE = regexp.MustCompile(
+		`(?s)\[\!\[[^\]]*\]\(([^\)]+)\)\]\(([^\)]+)\)`)
+	//                      11111         22222
+	//       `[![alt-text](image-URL)](link-url)`
+	// 1 : local image URL
+	// 2 : remote page URL
+
 	// R/O RegEx to extract link-text and link-URL from markup.
 	pvLinksRE = regexp.MustCompile(`(?s)\[([^\)]+)\]\s*\(([^\]]+)\)`)
-	//                                    1              2
+	//                                    11111          22222222
 	// 1 : link text
-	// 2 : link URL
+	// 2 : remote link URL
 
-	// RegEx to check whether an URL starts with a scheme.
+	// R/O RegEx to check whether an URL starts with a scheme.
 	pvSchemeRE = regexp.MustCompile(`^\w+:`)
 )
 
-// goSetPostingLinkViews sets the external links in `aPosting`
-// to include a page preview image (if available).
+type (
+	// tImgURL represents a pair of image name and page URL.
+	tImgURL struct {
+		imgURL  string
+		pageURL string
+	}
+	tImgURLlist []tImgURL
+)
+
+// checkForImgURL tests whether `aTxt` contains an external link with an
+// embedded page preview image, returning a list of the two link URLs.
 //
-//	`aPosting` is the path/filename of the posting to process.
-//	`aImageDirectory` is the URL directory for page preview images.
-func goSetPostingLinkViews(aPostName, aImageDirectory string) {
-	if 0 == len(aPostName) {
-		return
-	}
-	fName := filepath.Base(aPostName)
-	id := fName[:len(fName)-3] // strip name extension
-	p := NewPosting(id)
-	txt := p.Markdown()
-	if 0 == len(txt) {
-		return
-	}
-
-	linkMatches := pvLinksRE.FindAll(txt, -1)
-	if (nil == linkMatches) || (0 == len(linkMatches)) {
-
-		//TODO check for image URL constructs and make sure
-		// the referenced file does in fact exist.
-		return
-	}
-	for l, cnt := len(linkMatches), 0; cnt < l; cnt++ {
-		linkParts := pvLinksRE.FindSubmatch(linkMatches[cnt])
-		linkTxt, linkURL := string(linkParts[1]), string(linkParts[2])
-		if !pvSchemeRE.MatchString(linkURL) {
-			continue // skip local links
+//	`aTxt` is the text to search.
+func checkForImgURL(aTxt []byte) (rList tImgURLlist) {
+	matches := pvImageRE.FindAllSubmatch(aTxt, -1)
+	for idx := 0; idx < len(matches); idx++ {
+		pair := tImgURL{
+			filepath.Base(string(matches[idx][1])),
+			string(matches[idx][2]),
 		}
+		rList = append(rList, pair)
+	}
+	return
+} // checkForImgURL()
 
-		imgName, err := pageview.CreateImage(linkURL)
+// `goCheckPageImages()` checks whether the image file referenced
+// in the text of `aPosting` exists in `aImageDir`.
+//
+//	`aPosting` is the posting the text of which is searched for
+// a local image link.
+//	`aImageURLdir` is the local URL directory for page preview images.
+//	`aImageDir` is the directory used to store the generated images.
+func goCheckPageImages(aPosting *TPosting, aImageURLdir, aImageDir string) {
+	if nil == aPosting {
+		return
+	}
+	list := checkForImgURL(aPosting.Markdown())
+	if 0 == len(list) {
+		return
+	}
+
+	for _, pair := range list {
+		// `pageview.CreateImage()` checks whether the file exists
+		// and whether it's too old.
+		imgName, err := pageview.CreateImage(pair.pageURL)
 		if (nil != err) || (0 == len(imgName)) {
-			apachelogger.Err("goSetPostingLinkViews()",
-				fmt.Sprintf("pageview.CreateImage(%s): %v", linkURL, err))
-			// log.Printf("pageview.CreateImage(%s): %v", linkURL, err) //TODO REMOVE
+			apachelogger.Err("goCheckPageImage()",
+				fmt.Sprintf("pageview.CreateImage(%s): %v", pair.pageURL, err))
+			log.Printf("pageview.CreateImage(%s): %v",
+				pair.pageURL, err) //TODO REMOVE
 			continue
 		}
-		imgName = filepath.Join(aImageDirectory, imgName)
+	}
+} // goCheckPageImages()
 
-		// replace `[link-text](link-url)` by
-		// `[![link-text](image-URL)](link-url)`
-		search := regexp.QuoteMeta(string(linkParts[0]))
-		if re, err := regexp.Compile(search); nil == err {
-			replace := "[![" + linkTxt + "](/" + imgName + ")](" + linkURL + ")"
-			txt = re.ReplaceAllLiteral(txt, []byte(replace))
-			_, _ = p.Set(txt).Store()
-		}
+// `goPreparePost()` creates an preview image and updates `aLinkMatch`
+// in the text of `aPosting` to embedd a link to the image.
+//
+//	`aPosting` is the posting the text of which is going to be updated.
+//	`aLinkMatch` is the matchd link markdown text.
+//	`aImageURLdir` is the URL directory for page preview images.
+func goPreparePost(aPosting *TPosting, aLinkMatch []byte, aImageURLdir string) {
+	linkParts := pvLinksRE.FindSubmatch(aLinkMatch)
+	linkTxt, linkURL := string(linkParts[1]), string(linkParts[2])
+	if !pvSchemeRE.MatchString(linkURL) {
+		return // skip local links
+	}
+
+	imgName, err := pageview.CreateImage(linkURL)
+	if (nil != err) || (0 == len(imgName)) {
+		apachelogger.Err("goPreparePost()",
+			fmt.Sprintf("pageview.CreateImage(%s): %v", linkURL, err))
+		log.Printf("pageview.CreateImage(%s): %v", linkURL, err) //TODO REMOVE
+		return
+	}
+
+	// replace `[link-text](link-url)` by
+	// `[![alt-text](image-URL)](link-url)`
+	search := regexp.QuoteMeta(string(linkParts[0]))
+	if re, err := regexp.Compile(search); nil == err {
+		replace := "[![" +
+			linkTxt +
+			"](/" +
+			filepath.Join(aImageURLdir, imgName) +
+			")](" +
+			linkURL +
+			")"
+		txt := aPosting.Markdown()
+		txt = re.ReplaceAllLiteral(txt, []byte(replace))
+		_, _ = aPosting.Set(txt).Store()
+	}
+} // goPreparePost()
+
+// `goSetPostingLinkViews()` sets the external links in the text of
+// `aPosting` to include a page preview image (if available).
+//
+//	`aPosting` is the posting the text of which is going to be processed.
+//	`aImageURLdir` is the URL directory for page preview images.
+//	`aImageDir` is the directory used to store the generated images.
+func goSetPostingLinkViews(aPosting *TPosting, aImageURLdir, aImageDir string) {
+	if (nil == aPosting) || (0 == aPosting.Len()) {
+		return
+	}
+
+	linkMatches := pvLinksRE.FindAll(aPosting.Markdown(), -1)
+	if (nil == linkMatches) || (0 == len(linkMatches)) {
+		// Here we didn't find any normal links so check
+		// the links with an embedded page preview image.
+		go goCheckPageImages(aPosting, aImageURLdir, aImageDir)
+		return
+	}
+
+	for l, cnt := len(linkMatches), 0; cnt < l; cnt++ {
+		go goPreparePost(aPosting, linkMatches[cnt], aImageURLdir)
 	}
 } // goSetPostingLinkViews()
 
-// `goUpdateAllLinkViews()` prepares the external links in all postings
+// `goUpdateAllLinkPreviews()` prepares the external links in all postings
 // to use a page preview image (if available).
 //
-//	`aPostingBaseDir` is the base directory used for
-// storing articles/postings.
-//	`aImageDirectory` is the URL directory for page preview images.
-func goUpdateAllLinkViews(aPostingBaseDir, aImageDirectory string) {
+//	`aPostingBaseDir` is the base directory used for storing
+// articles/postings.
+//	`aImageURLdir` is the local URL directory for page preview images.
+//	`aImageDir` is the directory used to store the generated images.
+func goUpdateAllLinkPreviews(aPostingBaseDir, aImageURLdir, aImageDir string) {
 	dirnames, err := filepath.Glob(aPostingBaseDir + "/*")
 	if nil != err {
 		return // we can't recover from this :-(
 	}
+
 	for _, mdName := range dirnames {
 		filenames, err := filepath.Glob(mdName + "/*.md")
 		if nil != err {
@@ -99,30 +176,33 @@ func goUpdateAllLinkViews(aPostingBaseDir, aImageDirectory string) {
 			continue // skip empty directory
 		}
 		for _, postName := range filenames {
-			go goSetPostingLinkViews(postName, aImageDirectory)
+			fName := filepath.Base(postName)
+			go goSetPostingLinkViews(
+				NewPosting(fName[:len(fName)-3]), // strip name extension
+				aImageURLdir, aImageDir)
 		}
 	}
-} // goUpdateAllLinkViews()
+} // goUpdateAllLinkPreviews()
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 // InitPageImages starts the process to update the preview images
 // in all postings.
 //
-//	`aPostingBaseDir` is the base directory used for
-// storing articles/postings.
-//	`aImageDirectory` is the URL directory for page preview images.
-func InitPageImages(aPostingBaseDir, aImageDirectory string) {
-	go goUpdateAllLinkViews(aPostingBaseDir, aImageDirectory)
+//	`aPostingBaseDir` is the base directory used for storing
+// articles/postings.
+//	`aImageURLdir` is the URL directory for page preview images.
+func InitPageImages(aPostingBaseDir, aImageURLdir string) {
+	go goUpdateAllLinkPreviews(aPostingBaseDir, aImageURLdir, pageview.ImageDirectory())
 } // InitPageImages()
 
 // SetPostingLinkViews updates the external links in `aPosting`
 // to include a page preview image (if available).
 //
-//	`aPosting` is the path/filename of the posting to process.
-//	`aImageDirectory` is the URL directory for page preview images.
-func SetPostingLinkViews(aPostName, aImageDirectory string) {
-	goSetPostingLinkViews(aPostName, aImageDirectory)
+//	`aPosting` is the posting the text of which is going to be processed.
+//	`aImageURLdir` is the URL directory for page preview images.
+func SetPostingLinkViews(aPosting *TPosting, aImageURLdir string) {
+	goSetPostingLinkViews(aPosting, aImageURLdir, pageview.ImageDirectory())
 } // SetPostingLinkViews()
 
 /* _EoF_ */
--- pageview_test.go
+++ pageview_test.go
@@ -6,34 +6,112 @@
 
 package nele
 
-//lint:file-ignore ST1017 - I prefer Yoda conditions
-
 import (
+	"reflect"
 	"testing"
 
 	"github.com/mwat56/pageview"
 )
 
-func Test_goSetPostingLinkViews(t *testing.T) {
-	p1 := ``
-	p2 := `/home/matthias/devel/Go/src/github.com/mwat56/Nele/postings/201915d/15d9c2334fce3991.md`
-	imgDir := "./img/"
-	pageview.SetCacheDirectory(imgDir)
-	pageview.SetMaxAge(1)
+func Test_checkForImgURL(t *testing.T) {
+	var t1 []byte
+	var l1 tImgURLlist
+	t2 := []byte(`bla \n> [![„Wir sind alle Opfer hier“](/img/httpswwwaddendumorgnewsopferstudium.png)](https://www.addendum.org/news/opferstudium/)\n bla`)
+	l2 := tImgURLlist{
+		tImgURL{
+			`httpswwwaddendumorgnewsopferstudium.png`,
+			`https://www.addendum.org/news/opferstudium/`,
+		},
+	}
+	t3 := []byte(`bla \n> [![„Wir sind alle Opfer hier“](/img/httpswwwaddendumorgnewsopferstudium.png)](https://www.addendum.org/news/opferstudium/)\n bla\n> [![„Radikal den Kontakt abbrechen“](/img/httpswwwspiegeldepanoramahaeuslichegewaltwennmuttergeschlagenwirdwasmachtdasmitdenkinderna1291534html.png)](https://www.spiegel.de/panorama/haeusliche-gewalt-wenn-mutter-geschlagen-wird-was-macht-das-mit-den-kindern-a-1291534.html)`)
+	l3 := tImgURLlist{
+		tImgURL{
+			`httpswwwaddendumorgnewsopferstudium.png`,
+			`https://www.addendum.org/news/opferstudium/`,
+		},
+		tImgURL{
+			`httpswwwspiegeldepanoramahaeuslichegewaltwennmuttergeschlagenwirdwasmachtdasmitdenkinderna1291534html.png`,
+			`https://www.spiegel.de/panorama/haeusliche-gewalt-wenn-mutter-geschlagen-wird-was-macht-das-mit-den-kindern-a-1291534.html`,
+		},
+	}
+	t4 := []byte(`> @Google holt sich [Anti-Gewerkschafts-Beratung](https://www.heise.de/newsticker/meldung/Google-holt-sich-Anti-Gewerkschafts-Beratung-4593692.html?view=print).`)
+
 	type args struct {
-		aPosting, aImageDirectory string
+		aTxt []byte
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantRList tImgURLlist
+	}{
+		// TODO: Add test cases.
+		{" 1", args{t1}, l1},
+		{" 2", args{t2}, l2},
+		{" 3", args{t3}, l3},
+		{" 4", args{t4}, l1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRList := checkForImgURL(tt.args.aTxt); !reflect.DeepEqual(gotRList, tt.wantRList) {
+				t.Errorf("checkForImgURL() = %v,\nwant %v", gotRList, tt.wantRList)
+			}
+		})
+	}
+} // Test_checkForImgURL()
+
+func Test_goCheckPageImages(t *testing.T) {
+	pageview.SetImageDirectory("/tmp/")
+	pageview.SetMaxAge(1)
+	pageImgDir := pageview.ImageDirectory()
+	imgURLdir := "/img/"
+	var p1 TPosting
+	p2 := NewPosting("15d9c2334fce3991")
+	p3 := NewPosting("15d9393f4f5f3bb4")
+
+	type args struct {
+		aPosting     *TPosting
+		aImageURLdir string
+		aImageDir    string
 	}
 	tests := []struct {
 		name string
 		args args
 	}{
 		// TODO: Add test cases.
-		{" 1", args{p1, imgDir}},
-		{" 2", args{p2, imgDir}},
+		{" 1", args{&p1, imgURLdir, pageImgDir}},
+		{" 2", args{p2, imgURLdir, pageImgDir}},
+		{" 3", args{p3, imgURLdir, pageImgDir}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			goSetPostingLinkViews(tt.args.aPosting, tt.args.aImageDirectory)
+			goCheckPageImages(tt.args.aPosting, tt.args.aImageURLdir, tt.args.aImageDir)
+		})
+	}
+} // Test_goCheckPageImages()
+
+func Test_goSetPostingLinkViews(t *testing.T) {
+	pageview.SetImageDirectory("/tmp/")
+	pageview.SetMaxAge(1)
+	pageImgDir := pageview.ImageDirectory()
+	imgURLdir := "/img/"
+	var p1 TPosting
+	p2 := NewPosting("15d9c2334fce3991")
+	type args struct {
+		aPosting        *TPosting
+		aImageDirectory string
+		aCacheDirectory string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{" 1", args{&p1, imgURLdir, pageImgDir}},
+		{" 2", args{p2, imgURLdir, pageImgDir}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			goSetPostingLinkViews(tt.args.aPosting, tt.args.aImageDirectory, tt.args.aCacheDirectory)
 		})
 	}
 } // Test_goSetPostingLinkViews()
