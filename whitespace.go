dc911ce7fbf0e5b7fb8ed98762fe216d6b9e8824
Matthias Watermann <support@mwat.de>
2019-12-26 21:45:26 +0100

    * compacted variable initialisation;

--- whitespace.go
+++ whitespace.go
@@ -22,9 +22,9 @@ import (
 // `trimPREmatches()` removes leading/trailing whitespace from list entries.
 func trimPREmatches(aList [][]byte) [][]byte {
 	for idx, hit := range aList {
-		if aList[idx] = bytes.TrimSpace(hit); nil == aList[idx] {
-			aList[idx] = []byte(``)
-		}
+		aList[idx] = bytes.TrimSpace(hit)
+		// Since a list entry here can never be completely empty we
+		// don't have to check for `nil` after `bytes.TrimSpace()`.
 	}
 
 	return aList
@@ -34,7 +34,6 @@ func trimPREmatches(aList [][]byte) [][]byte {
 // the `RemoveWhiteSpace()` function.
 type (
 	tReItem struct {
-		search  string
 		replace string
 		regEx   *regexp.Regexp
 	}
@@ -44,48 +43,37 @@ var (
 	// RegEx to find PREformatted parts in an HTML page.
 	wsPreRE = regexp.MustCompile(`(?si)\s*<pre[^>]*>.*?</pre>\s*`)
 
-	// List of regular expressions matching different sets of HTML whitespace.
 	wsREs = []tReItem{
-		// comments
-		{`(?s)<!--.*?-->`, ``, nil},
+		// comments:
+		{``, regexp.MustCompile(`(?s)<!--.*?-->`)},
 		// HTML and HEAD elements:
-		{`(?i)\s*(</?(body|\!DOCTYPE|head|html|link|meta|script|style|title)[^>]*>)\s*`, `$1`, nil},
+		{`$1`, regexp.MustCompile(`(?si)\s*(</?(body|\!DOCTYPE|head|html|link|meta|script|style|title)[^>]*>)\s*`)},
 		// block elements:
-		{`(?i)\s*(</?(article|blockquote|div|footer|h[1-6]|header|nav|p|section)[^>]*>)\s*`, `$1`, nil},
+		{`$1`, regexp.MustCompile(`(?si)\s+(</?(article|blockquote|div|footer|h[1-6]|header|nav|p|section)[^>]*>)`)},
+		{`$1`, regexp.MustCompile(`(?si)(</?(article|blockquote|div|footer|h[1-6]|header|nav|p|section)[^>]*>)\s+`)},
 		// lists:
-		{`(?i)\s*(</?([dou]l|li|d[dt])[^>]*>)\s*`, `$1`, nil},
-		// table:
-		{`(?i)\s*(</?(col|t(able|body|foot|head|[dhr]))[^>]*>)\s*`, `$1`, nil},
-		// forms:
-		{`(?i)\s*(</?(form|fieldset|legend|opt(group|ion))[^>]*>)\s*`, `$1`, nil},
+		{`$1`, regexp.MustCompile(`(?si)\s+(</?([dou]l|li|d[dt])[^>]*>)`)},
+		{`$1`, regexp.MustCompile(`(?si)(</?([dou]l|li|d[dt])[^>]*>)\s+`)},
+		// table elements:
+		{`$1`, regexp.MustCompile(`(?si)\s+(</?(col|t(able|body|foot|head|[dhr]))[^>]*>)`)},
+		{`$1`, regexp.MustCompile(`(?si)(</?(col|t(able|body|foot|head|[dhr]))[^>]*>)\s+`)},
+		// form elements:
+		{`$1`, regexp.MustCompile(`(?si)\s+(</?(form|fieldset|legend|opt(group|ion))[^>]*>)`)},
+		{`$1`, regexp.MustCompile(`(?si)(</?(form|fieldset|legend|opt(group|ion))[^>]*>)\s+`)},
 		// BR / HR:
-		{`(?i)\s*(<[bh]r[^>]*>)\s*`, `$1`, nil},
+		{`$1`, regexp.MustCompile(`(?i)\s*(<[bh]r[^>]*>)\s*`)},
 		// whitespace after opened anchor:
-		{`(?i)(<a\s+[^>]*>)\s+`, `$1`, nil},
+		{`$1`, regexp.MustCompile(`(?si)(<a\s+[^>]*>)\s+`)},
 		// preserve empty table cells:
-		{`(?i)(<td(\s+[^>]*)?>)\s+(</td>)`, `$1&#160;$3`, nil},
+		{`$1&#160;$3`, regexp.MustCompile(`(?i)(<td(\s+[^>]*)?>)\s+(</td>)`)},
 		// remove empty paragraphs:
-		{`(?i)<(p)(\s+[^>]*)?>\s*</$1>`, ``, nil},
+		{``, regexp.MustCompile(`(?i)<(p)(\s+[^>]*)?>\s*</$1>`)},
 		// whitespace before closing GT:
-		{`\s+>`, `>`, nil},
+		{`>`, regexp.MustCompile(`\s+>`)},
 	}
 )
 
-var (
-	// Initialise the `whitespaceREs` list.
-	_ = func() int {
-		result := 0
-		for idx, re := range wsREs {
-			wsREs[idx].regEx = regexp.MustCompile(re.search)
-			result++
-		}
-		result++
-
-		return result
-	}()
-)
-
-// RemoveWhiteSpace returns `aPage` with al.l HTML comments and
+// RemoveWhiteSpace returns `aPage` with all HTML comments and
 // unnecessary whitespace removed.
 //
 // This function removes all unneeded/redundant whitespace
@@ -97,7 +85,6 @@ var (
 func RemoveWhiteSpace(aPage []byte) []byte {
 	var repl, search string
 
-	// fmt.Println("Page0:", string(aPage))
 	// (0) Check whether there are PREformatted parts:
 	preMatches := wsPreRE.FindAll(aPage, -1)
 	if (nil == preMatches) || (0 >= len(preMatches)) {
@@ -118,13 +105,11 @@ func RemoveWhiteSpace(aPage []byte) []byte {
 			aPage = re.ReplaceAllLiteral(aPage, []byte(repl))
 		}
 	}
-	// fmt.Println("Page1:", string(aPage))
 
 	// (2) traverse through all the whitespace REs:
 	for _, re := range wsREs {
 		aPage = re.regEx.ReplaceAll(aPage, []byte(re.replace))
 	}
-	// fmt.Println("Page2:", string(aPage))
 
 	// (3) replace the PRE dummies with the real markup:
 	for lLen, cnt := len(preMatches), 0; cnt < lLen; cnt++ {
@@ -133,7 +118,6 @@ func RemoveWhiteSpace(aPage []byte) []byte {
 			aPage = re.ReplaceAllLiteral(aPage, preMatches[cnt])
 		}
 	}
-	// fmt.Println("Page3:", string(aPage))
 
 	return aPage
 } // RemoveWhiteSpace()
