da586910afa45c49fd89695cb9df9ebd55c92d0b
Matthias Watermann <support@mwat.de>
2020-03-01 18:11:26 +0100

    * updated file;

--- go.mod
+++ go.mod
@@ -7,15 +7,13 @@ require (
 	github.com/mwat56/apachelogger v1.4.5
 	github.com/mwat56/cssfs v0.2.1
 	github.com/mwat56/errorhandler v1.1.5
-	github.com/mwat56/hashtags v0.4.15
+	github.com/mwat56/hashtags v0.4.16
 	github.com/mwat56/ini v1.3.8
 	github.com/mwat56/jffs v0.0.5
-	github.com/mwat56/pageview v0.4.2
-	github.com/mwat56/passlist v1.3.0
-	github.com/mwat56/uploadhandler v1.1.1
+	github.com/mwat56/pageview v0.4.3
+	github.com/mwat56/passlist v1.3.1
+	github.com/mwat56/uploadhandler v1.1.2
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d // indirect
-	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
 	gopkg.in/russross/blackfriday.v2 v2.0.1
 )
 
--- posting_test.go
+++ posting_test.go
@@ -191,8 +191,8 @@ func TestPostingCount(t *testing.T) {
 		wantRCount int
 	}{
 		// TODO: Add test cases.
-		{" 1", 1068},
-		{" 2", 1068},
+		{" 1", 1113},
+		{" 2", 1113},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -335,7 +335,7 @@ func TestTPosting_Delete(t *testing.T) {
 	id2 := newID(time.Date(2019, 5, 4, 0, 0, 0, 0, time.Local))
 	p2 := NewPosting(id2).
 		Set([]byte("just a dummy"))
-	_,_ = p2.Store() // create a file
+	_, _ = p2.Store() // create a file
 	type fields struct {
 		p *TPosting
 	}
@@ -398,7 +398,7 @@ func TestTPosting_Exists(t *testing.T) {
 	p2 := NewPosting(id2)
 	id3 := newID(time.Date(2019, 1, 1, 0, 0, 0, 3, time.Local))
 	p3 := NewPosting(id3).Set([]byte("Hello World"))
-	_,_ = p3.Store()
+	_, _ = p3.Store()
 	type fields struct {
 		id       string
 		markdown []byte
@@ -433,7 +433,7 @@ func TestTPosting_Load(t *testing.T) {
 	id2 := newID(time.Date(2019, 5, 4, 0, 0, 0, 0, time.Local))
 	md2 := []byte("Load: this is more nonsense")
 	p2 := NewPosting(id2).Set(md2)
-	_,_ = p2.Store()
+	_, _ = p2.Store()
 	p2.Clear()
 	type fields struct {
 		p *TPosting
