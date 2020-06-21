36c8d365a1b78ce7434dfda84255fbe61af617cf
Matthias Watermann <support@mwat.de>
2020-06-21 22:23:07 +0200

    + implemented `TemplateData.Get()`;

--- templatedata.go
+++ templatedata.go
@@ -1,5 +1,5 @@
 /*
-   Copyright © 2019 M.Watermann, 10247 Berlin, Germany
+   Copyright © 2019, 2020 M.Watermann, 10247 Berlin, Germany
               All rights reserved
           EMail : <support@mwat.de>
 */
@@ -18,14 +18,24 @@ type (
 	TemplateData map[string]interface{}
 )
 
+// Get returns the value associated with `aKey` and `true`.
+// If `aKey` is not present in the list then the `bool` return
+// value will be `false`.
+//
+//	`aKey` The value's identifier (as used as placeholder in the template).
+func (dl TemplateData) Get(aKey string) (rValue interface{}, rOK bool) {
+	rValue, rOK = dl[aKey]
+
+	return
+} // Get()
+
 // Set inserts `aValue` identified by `aKey` to the list.
 //
 // If there's already a list entry with `aKey` its current value
 // gets replaced by `aValue`.
 //
-// `aKey` is the values's identifier (as used as placeholder in the template).
-//
-// `aValue` contains the data entry's value.
+//	`aKey` The value's identifier (as used as placeholder in the template).
+//	`aValue` The data entry's value.
 func (dl *TemplateData) Set(aKey string, aValue interface{}) *TemplateData {
 	(*dl)[aKey] = aValue
 
--- templatedata_test.go
+++ templatedata_test.go
@@ -6,8 +6,6 @@
 
 package nele
 
-//lint:file-ignore ST1017 - I prefer Yoda conditions
-
 import (
 	"reflect"
 	"testing"
@@ -30,6 +28,41 @@ func TestNewTemplateData(t *testing.T) {
 		})
 	}
 } // TestNewTemplateData()
+
+func TestTemplateData_Get(t *testing.T) {
+	d1 := NewTemplateData()
+	(*d1)[`key1`] = `val1`
+	(*d1)[`key3`] = false
+
+	type args struct {
+		aKey string
+	}
+	tests := []struct {
+		name       string
+		dl         TemplateData
+		args       args
+		wantRValue interface{}
+		wantROK    bool
+	}{
+		// TODO: Add test cases.
+		{" 1", *d1, args{`key1`}, `val1`, true},
+		{" 2", *d1, args{`key2`}, `val2`, false},
+		{" 3", *d1, args{`key3`}, false, true},
+		{" 4", *d1, args{`key3`}, true, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotRValue, gotROK := tt.dl.Get(tt.args.aKey)
+			if gotROK != tt.wantROK {
+				t.Errorf("TemplateData.Get() gotROK = %v, want %v", gotROK, tt.wantROK)
+				return
+			}
+			if gotROK && !reflect.DeepEqual(gotRValue, tt.wantRValue) {
+				t.Errorf("TemplateData.Get() gotRValue = %v,\nwant %v", gotRValue, tt.wantRValue)
+			}
+		})
+	}
+} // TestTemplateData_Get()
 
 func TestTemplateData_Set(t *testing.T) {
 	d1 := NewTemplateData()
