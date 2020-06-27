eb1ef0fc41617d57175692951f69fde125a6083b
Matthias Watermann <support@mwat.de>
2020-06-27 15:11:06 +0200

    * updated file;

--- posting_test.go
+++ posting_test.go
@@ -104,8 +104,8 @@ func TestPostingCount(t *testing.T) {
 		wantRCount uint32
 	}{
 		// TODO: Add test cases.
-		{" 1", 1435},
-		{" 2", 1435},
+		{" 1", 1456},
+		{" 2", 1456},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
